437cca3bc78c123264470ca489682676cdbb3f61
Cristian Dias de Castro <cristian.dias@datalogue.de>
2023-07-18 16:21:38 +0200

    wip

--- RandomWord.java
+++ RandomWord.java
@@ -4,9 +4,16 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
   public static void main(String[] args) {
-    String inpuString = StdIn.readString();
-    if (!StdIn.isEmpty() && StdRandom.bernoulli()) {
-      StdOut.println(inpuString);
+    int count = 0;
+    String champion = "";
+
+    while (!StdIn.isEmpty()) {
+      String nextWord = StdIn.readString();
+      count++;
+      if (StdRandom.bernoulli(1.0/count)) {
+        champion = nextWord;
+      }
     }
+    StdOut.println(champion);
   }
 }
